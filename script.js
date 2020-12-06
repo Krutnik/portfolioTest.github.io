5181e7fb12938e93f5acf2a1282aee0c013ca5d7
Кирилл <67455038+Krutnik@users.noreply.github.com>
2020-12-06 17:09:10 +0300

    upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let textMesseng = document.querySelector('.messeng');
+let mainDisplay = document.querySelector('.main');
+let btnSendMesseng = document.querySelector('.btn');
+let newMesseng;
+let yourName;
+let textNewMesseng;
+
+function createElem() {
+	newMesseng = document.createElement('div');
+	newMesseng.classList.add('.user-two');
+	
+	yourName = document.createElement('p');
+	yourName.textContent = "Petrov Petr";
+	newMesseng.appendChild(yourName);
+	
+	textNewMesseng = document.createElement('div');
+	textNewMesseng.classList.add('.text');
+	textNewMesseng.textContent = textMesseng.value;
+	newMesseng.appendChild(textNewMesseng);
+	
+	return newMesseng;
+} 
+
+btnSendMesseng.onclick = function() {
+	mainDisplay.appendChild(newMesseng());
+}
